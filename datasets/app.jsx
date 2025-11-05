d7db07997a8252b8fcad6edd89625550171b441f
lautaroadelfino-hub <lautaroadelfino@gmail.com>
2025-11-05 18:36:01 -0300

    Delete datasets directory

--- datasets/app.jsx
+++
@@ -1,9 +0,0 @@
-import CalculadoraSueldoTandil from './CalculadoraSueldoTandil';
-
-export default function App() {
-  return (
-    <div className="min-h-screen bg-slate-100">
-      <CalculadoraSueldoTandil />
-    </div>
-  );
-}
