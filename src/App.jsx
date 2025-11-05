c69baaa8a134208d008c30d38f6dbc5dd4235a32
lautaroadelfino-hub <lautaroadelfino@gmail.com>
2025-11-05 18:34:43 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import CalculadoraSueldoTandil from './CalculadoraSueldoTandil';
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-slate-100">
+      <CalculadoraSueldoTandil />
+    </div>
+  );
+}
